1382084efead8127069ae9f56952d82181de4e60
Hunter Sides <hunter.s.1337@gmail.com>
2020-08-10 21:51:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,12 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  def self.create
+  end 
+  
+  def drop_table
+  end
+  def save 
+  end
+  
 end
